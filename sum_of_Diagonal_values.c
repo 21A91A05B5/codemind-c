0783071493ceb7004d9f356f26abadf88e52ea15
21A91A05B5 <97424445+21A91A05B5@users.noreply.github.com>
2022-04-30 22:08:37 +0530

    #include<stdio.h> int main() {     int r,c,i,j,arr[100][100],s1=0;     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&arr[i][j]);         }     }     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             if(i==j || i+j==c-1)             {                 s1=s1+arr[i][j];             }         }     }     printf("%d",s1); }

---
+++ sum_of_Diagonal_values.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int r,c,i,j,arr[100][100],s1=0;
+    scanf("%d%d",&r,&c);
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            if(i==j || i+j==c-1)
+            {
+                s1=s1+arr[i][j];
+            }
+        }
+    }
+    printf("%d",s1);
+}
